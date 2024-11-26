fd91f22264aa8ffd7f5f17616182fbe4322504b5
František Hluchník <frantahluchnik@gmail.com>
2024-11-26 23:46:59 +0100

    fix staticfiles

--- cantica/settings.py
+++ cantica/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG_VALUE', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', 'cantica.fly.dev', 'cantica.hluchnikovi.cz']
+ALLOWED_HOSTS = ['localhost', 'cantica.fly.dev', 'cantica.hluchnikovi.cz', 'directuscantica.cz']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://cantica.fly.dev',
     'https://cantica.hluchnikovi.cz',
+    'https://directuscantica.cz',
 ]
 
 
@@ -54,13 +55,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cantica.urls'
@@ -143,8 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
